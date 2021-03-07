f4106a1b3d75bdb5740209e66b2df1a2c3cc7c7c
AbirHamzi <abir.hamzi@pocteo.co>
2021-03-07 12:40:02 +0100

    fix dependencies

--- go.mod
+++ go.mod
@@ -1,53 +1,67 @@
 module github.com/bpftools/kube-bpf
 
+go 1.15
+
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
+	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/emicklei/go-restful v2.15.0+incompatible // indirect
+	github.com/evanphx/json-patch v4.9.0+incompatible // indirect
 	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
 	github.com/go-playground/locales v0.11.2 // indirect
 	github.com/go-playground/universal-translator v0.16.0 // indirect
-	github.com/gogo/protobuf v1.2.0 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
-	github.com/golang/groupcache v0.0.0-20180924190550-6f2cf27854a4 // indirect
-	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
-	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
-	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/googleapis/gnostic v0.4.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
-	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/iovisor/gobpf v0.0.0-20190202095358-248028b4f94b
-	github.com/json-iterator/go v1.1.5 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/iovisor/gobpf v0.1.0
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pty v1.1.5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
-	github.com/pkg/errors v0.8.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v0.9.2
 	github.com/safchain/insanelock v0.0.0-20180509135444-33bca4586648 // indirect
 	github.com/skydive-project/skydive v0.21.0
 	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.3
+	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.3.1
+	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/vishvananda/netlink v1.0.0
 	github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc // indirect
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1
-	golang.org/x/sys v0.0.0-20181228144115-9a3f9b0469bb // indirect
-	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	google.golang.org/genproto v0.0.0-20201019141844-1ed22bb0c154 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.17.1
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	k8s.io/api v0.0.0-20180904230853-4e7be11eab3f
+	k8s.io/api v0.19.0-alpha.1
 	k8s.io/apiextensions-apiserver v0.0.0-20180910084140-b05d9bb7cc74
-	k8s.io/apimachinery v0.0.0-20180904193909-def12e63c512
-	k8s.io/client-go v2.0.0-alpha.0.0.20180910083459-2cefa64ff137+incompatible
+	k8s.io/apimachinery v0.19.0-alpha.1
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/gengo v0.0.0-20200413195148-3a45101e95ac // indirect
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/klog/v2 v2.4.0 // indirect
 	k8s.io/kubernetes v1.11.3
 	k8s.io/utils v0.0.0-20190221042446-c2654d5206da
+	sigs.k8s.io/structured-merge-diff/v4 v4.0.2 // indirect
 )
 
 replace github.com/Sirupsen/logrus => github.com/sirupsen/logrus v1.2.0
--- vendor/github.com/iovisor/gobpf/elf/module.go
+++ vendor/github.com/iovisor/gobpf/elf/module.go
@@ -33,13 +33,25 @@ import (
 )
 
 /*
+#cgo CFLAGS: -I${SRCDIR}/include/uapi -I${SRCDIR}/include
+
 #include <unistd.h>
 #include <strings.h>
+#include <string.h>
 #include <stdlib.h>
-#include "include/bpf.h"
+#include <stdbool.h>
+#include <linux/bpf.h>
+#include "libbpf.h"
 #include <linux/perf_event.h>
 #include <linux/unistd.h>
 #include <sys/socket.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <errno.h>
+#include <net/if.h>
+#include <string.h>
+#include <linux/if_link.h>
+#include <linux/rtnetlink.h>
 
 static int perf_event_open_tracepoint(int tracepoint_id, int pid, int cpu,
                            int group_fd, unsigned long flags)
@@ -59,7 +71,7 @@ int bpf_prog_attach(int prog_fd, int target_fd, enum bpf_attach_type type)
 {
 	union bpf_attr attr;
 
-	bzero(&attr, sizeof(attr));
+	memset(&attr, 0, sizeof(attr));
 	attr.target_fd	   = target_fd;
 	attr.attach_bpf_fd = prog_fd;
 	attr.attach_type   = type;
@@ -71,7 +83,7 @@ int bpf_prog_detach(int prog_fd, int target_fd, enum bpf_attach_type type)
 {
 	union bpf_attr attr;
 
-	bzero(&attr, sizeof(attr));
+	memset(&attr, 0, sizeof(attr));
 	attr.target_fd	   = target_fd;
 	attr.attach_bpf_fd = prog_fd;
 	attr.attach_type   = type;
@@ -87,6 +99,26 @@ int bpf_attach_socket(int sock, int fd)
 int bpf_detach_socket(int sock, int fd)
 {
 	return setsockopt(sock, SOL_SOCKET, SO_DETACH_BPF, &fd, sizeof(fd));
+}
+
+int bpf_attach_xdp(const char *dev_name, int progfd, uint32_t flags)
+{
+  	int ifindex = if_nametoindex(dev_name);
+  	char err_buf[256];
+  	int ret = -1;
+
+  	if (ifindex == 0) {
+    		fprintf(stderr, "bpf: Resolving device name to index: %s\n", strerror(errno));
+    		return -1;
+  	}
+
+  	ret = bpf_set_link_xdp_fd(ifindex, progfd, flags);
+  	if (ret) {
+    		fprintf(stderr, "bpf: Attaching prog to %s: %s", dev_name, err_buf);
+    		return -1;
+  	}
+
+  	return 0;
 }
 */
 import "C"
@@ -104,6 +136,9 @@ type Module struct {
 	socketFilters      map[string]*SocketFilter
 	tracepointPrograms map[string]*TracepointProgram
 	schedPrograms      map[string]*SchedProgram
+	xdpPrograms        map[string]*XDPProgram
+
+	compatProbe bool // try to be automatically convert function names depending on kernel versions (SyS_ and __x64_sys_)
 }
 
 // Kprobe represents a kprobe or kretprobe and has to be declared
@@ -129,6 +164,8 @@ const (
 	EgressType
 	SockCreateType
 )
+
+const defaultLogSize uint32 = 524288
 
 // CgroupProgram represents a cgroup skb/sock program
 type CgroupProgram struct {
@@ -159,7 +196,14 @@ type SchedProgram struct {
 	fd    int
 }
 
-func newModule() *Module {
+// XDPProgram represents a XDP hook program
+type XDPProgram struct {
+	Name  string
+	insns *C.struct_bpf_insn
+	fd    int
+}
+
+func newModule(logSize uint32) *Module {
 	return &Module{
 		probes:             make(map[string]*Kprobe),
 		uprobes:            make(map[string]*Uprobe),
@@ -167,18 +211,33 @@ func newModule() *Module {
 		socketFilters:      make(map[string]*SocketFilter),
 		tracepointPrograms: make(map[string]*TracepointProgram),
 		schedPrograms:      make(map[string]*SchedProgram),
-		log:                make([]byte, 524288),
+		xdpPrograms:        make(map[string]*XDPProgram),
+		log:                make([]byte, logSize),
 	}
 }
 
+func NewModuleWithLog(fileName string, logSize uint32) *Module {
+
+	module := newModule(logSize)
+	module.fileName = fileName
+	return module
+}
+
+func NewModuleFromReaderWithLog(fileReader io.ReaderAt, logSize uint32) *Module {
+	module := newModule(logSize)
+	module.fileReader = fileReader
+	return module
+}
+
 func NewModule(fileName string) *Module {
-	module := newModule()
+
+	module := newModule(defaultLogSize)
 	module.fileName = fileName
 	return module
 }
 
 func NewModuleFromReader(fileReader io.ReaderAt) *Module {
-	module := newModule()
+	module := newModule(defaultLogSize)
 	module.fileReader = fileReader
 	return module
 }
@@ -262,6 +321,14 @@ func perfEventOpenTracepoint(id int, progFd int) (int, error) {
 // Log gives users access to the log buffer with verifier messages
 func (b *Module) Log() []byte {
 	return b.log
+}
+
+// EnableOptionCompatProbe will attempt to automatically convert function
+// names in kprobe and kretprobe to maintain compatibility between kernel
+// versions.
+// See: https://github.com/iovisor/gobpf/issues/146
+func (b *Module) EnableOptionCompatProbe() {
+	b.compatProbe = true
 }
 
 // EnableKprobe enables a kprobe/kretprobe identified by secName.
@@ -402,12 +469,27 @@ func (b *Module) IterTracepointProgram() <-chan *TracepointProgram {
 	return ch
 }
 
+func (b *Module) IterXDPProgram() <-chan *XDPProgram {
+	ch := make(chan *XDPProgram)
+	go func() {
+		for name := range b.xdpPrograms {
+			ch <- b.xdpPrograms[name]
+		}
+		close(ch)
+	}()
+	return ch
+}
+
 func (b *Module) CgroupProgram(name string) *CgroupProgram {
 	return b.cgroupPrograms[name]
 }
 
 func (p *CgroupProgram) Fd() int {
 	return p.fd
+}
+
+func (tp *TracepointProgram) Fd() int {
+	return tp.fd
 }
 
 var safeEventRegexp = regexp.MustCompile("[^a-zA-Z0-9]")
@@ -429,7 +511,7 @@ func AttachUprobe(uprobe *Uprobe, path string, offset uint64) error {
 		probeType, safeEventName(path), offset, os.Getpid())
 
 	if _, ok := uprobe.efds[eventName]; ok {
-		return fmt.Errorf("uprobe already attached")
+		return errors.New("uprobe already attached")
 	}
 
 	uprobeID, err := writeUprobeEvent(probeType, eventName, path, offset)
@@ -445,6 +527,51 @@ func AttachUprobe(uprobe *Uprobe, path string, offset uint64) error {
 	uprobe.efds[eventName] = efd
 
 	return nil
+}
+
+func (b *Module) AttachXDP(devName string, secName string) error {
+	xdp, ok := b.xdpPrograms[secName]
+	if !ok {
+		return fmt.Errorf("no such XDP hook %q", secName)
+	}
+	if err := attachXDP(devName, xdp.fd, 0, true); err != nil {
+		return err
+	}
+	return nil
+}
+
+// AttachXDPWithFlags attaches an xdp section to a device with flags.
+func (b *Module) AttachXDPWithFlags(devName string, secName string, flags uint32) error {
+	xdp, ok := b.xdpPrograms[secName]
+	if !ok {
+		return fmt.Errorf("no such XDP hook %q", secName)
+	}
+	return attachXDP(devName, xdp.fd, flags, true)
+}
+
+func (b *Module) RemoveXDP(devName string) error {
+	if err := attachXDP(devName, -1, 0, false); err != nil {
+		return err
+	}
+	return nil
+}
+
+func attachXDP(devName string, fd int, flags uint32, attach bool) error {
+	devNameCS := C.CString(devName)
+	res, err := C.bpf_attach_xdp(devNameCS, C.int(fd), C.uint32_t(flags))
+	defer C.free(unsafe.Pointer(devNameCS))
+
+	if res != 0 || err != nil {
+		return fmt.Errorf(xdpFormat(attach), devName, err)
+	}
+	return nil
+}
+
+func xdpFormat(attach bool) string {
+	if attach {
+		return "failed to attach BPF xdp to device %s: %v"
+	}
+	return "failed to remove BPF xdp from device %s: %v"
 }
 
 func AttachCgroupProgram(cgroupProg *CgroupProgram, cgroupPath string, attachType AttachType) error {
@@ -593,6 +720,14 @@ func (sp *SchedProgram) Fd() int {
 	return sp.fd
 }
 
+func (b *Module) XDPProgram(name string) *XDPProgram {
+	return b.xdpPrograms[name]
+}
+
+func (xdpp *XDPProgram) Fd() int {
+	return xdpp.fd
+}
+
 func (b *Module) closeProbes() error {
 	var funcName string
 	for _, probe := range b.probes {
@@ -673,6 +808,15 @@ func (b *Module) closeSocketFilters() error {
 	return nil
 }
 
+func (b *Module) closeXDPPrograms() error {
+	for _, xdp := range b.xdpPrograms {
+		if err := syscall.Close(xdp.fd); err != nil {
+			return fmt.Errorf("error closing XDP program fd: %v", err)
+		}
+	}
+	return nil
+}
+
 func unpinMap(m *Map, pinPath string) error {
 	mapPath, err := getMapPath(&m.m.def, m.Name, pinPath)
 	if err != nil {
@@ -703,7 +847,23 @@ func (b *Module) closeMaps(options map[string]CloseOptions) error {
 				return fmt.Errorf("error unpinning map %q: %v", m.Name, err)
 			}
 		}
+
+		// unmap
+		for _, base := range m.bases {
+			err := syscall.Munmap(base)
+			if err != nil {
+				return fmt.Errorf("unmap error: %v", err)
+			}
+		}
+
 		for _, fd := range m.pmuFDs {
+			// disable
+			_, _, err2 := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), C.PERF_EVENT_IOC_DISABLE, 0)
+			if err2 != 0 {
+				return fmt.Errorf("error disabling perf event: %v", err2)
+			}
+
+			// close
 			if err := syscall.Close(int(fd)); err != nil {
 				return fmt.Errorf("error closing perf event fd: %v", err)
 			}
@@ -730,6 +890,7 @@ type CloseOptions struct {
 // * Detaching BPF programs from kprobes and closing their file descriptors
 // * Closing cgroup-bpf file descriptors
 // * Closing socket filter file descriptors
+// * Closing XDP file descriptors
 //
 // It doesn't detach BPF programs from cgroups or sockets because they're
 // considered resources the user controls.
@@ -756,6 +917,9 @@ func (b *Module) CloseExt(options map[string]CloseOptions) error {
 		return err
 	}
 	if err := b.closeSocketFilters(); err != nil {
+		return err
+	}
+	if err := b.closeXDPPrograms(); err != nil {
 		return err
 	}
 	return nil
